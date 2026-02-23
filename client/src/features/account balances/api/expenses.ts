6dd2e06565b70dbc562091502f9a77e1832edcc1
Sonnumber5 <157999365+Sonnumber5@users.noreply.github.com>
2026-02-22 19:26:29 -0500

    Implement CRUD operations for expenses in the API

--- client/src/features/account balances/api/expenses.ts
+++ client/src/features/account balances/api/expenses.ts
@@ -1,6 +1,58 @@
+import type { ExpenseDTO } from "../types";
 import api from "./axios";
 
-export const 
+export const createExpense = (data: ExpenseDTO) => {
+    return api.post(`/expenses`, data);
+}
+
+export const getExpenses = (month: string) => {
+    return api.get(`/expenses?month=${month}`);
+}
+
+export const getExpenseById = (id: number) => {
+    return api.get(`/expenses/${id}`);
+}
+
+export const updateExpense = (id: number, data: ExpenseDTO) => {
+    return api.put(`/expenses/${id}`, data);
+}
+
+export const deleteExpense = (id: number) => {
+    return api.delete(`/expenses/${id}`);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
