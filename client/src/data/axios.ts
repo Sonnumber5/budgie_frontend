43d0a3ef0637417269e041dc33038c0442af2f63
Sonnumber5 <157999365+Sonnumber5@users.noreply.github.com>
2026-05-05 18:55:43 -0400

    feat: configure deployment settings for Heroku and update API base URL

--- client/src/data/axios.ts
+++ client/src/data/axios.ts
@@ -5,7 +5,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3001/api',
+    baseURL: import.meta.env.VITE_API_URL || 'https://budgie-backend-232e8daa5655.herokuapp.com/api',
     withCredentials: true
 });
 
