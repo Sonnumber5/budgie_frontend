74085b8fa75467f9a054607a0232b7f0c2df7301
Sonnumber5 <157999365+Sonnumber5@users.noreply.github.com>
2026-05-21 22:02:52 -0400

    Adjusted FundPreview layout/spacing; fund transaction button on FundPreview; updated placeholder opacity in input fields; adjust dashboard fund balance section height for better alignment

--- client/src/features/savings-funds/components/FundPreview.tsx
+++ client/src/features/savings-funds/components/FundPreview.tsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import type { SavingsFund } from "../../../types";
 import { formatCurrency } from "../../../utils/formatCurrency";
 import './FundPreview.css';
+import { Modal } from "../../../components/modal";
+import { FundTransactionForm } from "../../fund-transactions/components/FundTransactionForm";
 
 interface SavingsFundProps{
     fund: SavingsFund;
@@ -8,27 +11,30 @@ interface SavingsFundProps{
 
 // Renders a compact read-only preview of a savings fund showing its name, balance vs. goal, and a progress bar.
 export const FundPreview = ({ fund }: SavingsFundProps) => {
+    const [ isTransactionModalOpen, setIsTransactionModalOpen ] = useState(false);
 
-    
     const progress = Math.min((fund.balance / fund.goal) * 100, 100);
 
     return (
         <div className="fund-preview">
+                <Modal isOpen={isTransactionModalOpen} onClose={() => {setIsTransactionModalOpen(false)}} title="AddTransaction">
+                    <FundTransactionForm fundId={fund.id} onSuccess={() => {setIsTransactionModalOpen(false)}}/>
+                </Modal>
             <div className="fund-preview-header">
                 <div>ICON</div>
-                <div>{fund.name}</div>
+                <p>{fund.name}</p>
             </div>
             <div className="fund-preview-main-content">
+                <div className="progress-bar">
+                    {progress > 0 && <div className="progress-fill fund-preview" style={{ width: `${progress}%` }}/>}
+                </div>
                 <p>
                     <span>{formatCurrency(Number(fund.balance))}</span>
                     <span className="text-fraction"> / {formatCurrency(Number(fund.goal))}</span>
                 </p>
-                <div className="progress-bar">
-                    {progress > 0 && <div className="progress-fill fund-preview" style={{ width: `${progress}%` }}/>}
-                </div>
             </div>
             <div className="fund-preview-btn">
-                <button className="btn-primary">Add Transaction</button>
+                <button className="btn-primary" onClick={() => {setIsTransactionModalOpen(true)}}>Add Transaction</button>
             </div>
         </div>
     )
